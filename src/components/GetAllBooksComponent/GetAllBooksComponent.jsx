0a2b0bbf752eafd31b1caf6cb1250bbe0d2f2a94
Aishwarya T M <aishwaryamani2003@gmail.com>
2024-05-27 20:26:55 +0530

    initial commit

--- src/components/GetAllBooksComponent/GetAllBooksComponent.jsx
+++ src/components/GetAllBooksComponent/GetAllBooksComponent.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import BookComponent from './BookComponent'
 
 const GetAllBooksComponent = () => {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3500/api/v1/books')
+    .then(response => setBooks(response.data))
+    .catch(error => console.log(error))
+  }, [books])
+
   return (
-    <div>GetAllBooksComponent</div>
+    <div className='books'>
+      {books.map((book, index) => (
+        <BookComponent book = {book} key = {index}/>
+      ))}
+    </div>
   )
 }
 
